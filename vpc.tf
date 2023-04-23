157c4299c0d2a5f7b543732281bc016e04304c46
Mikael Fredriksson <224mikael@gmail.com>
2023-04-23 11:42:50 +0000

    Bugfix

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
-module "vpc" {
-    source  = "terraform-google-modules/network/google"
+module "network" {
+    source  = "app.terraform.io/mikael-chip-training/network/google"
     version = "~> 3.0"
 
     project_id   = var.project
